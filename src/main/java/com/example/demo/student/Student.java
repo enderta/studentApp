6d555031b307dd4c7954d7467f47bac46972a82f
enderta <endertanver@gmail.com>
2022-11-11 15:27:03 +0000

    student

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,27 @@
+package com.example.demo.student;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table
+public class Student {
+	@Id
+	@SequenceGenerator(
+			name = "student_sequence",
+			sequenceName = "student_sequence",
+			allocationSize = 1
+	)
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "student_sequence"
+	)
+	private Long id;
+	private String name;
+	private String email;
+	private LocalDate dob;
+	private Integer age;
+}
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,21 @@
+/*
+package com.example.demo.student;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Component
+@Service
+public class StudentService {
+public List<Student> getStudents() {
+	return new ArrayList<>();
+}
+
+
+}*/
